7198ff515c9c589fba2cfb7c72dd04b5be8556c0
devender.mishra <devender.mishra@moonshinetechnology.com>
2025-11-30 21:48:22 +0530

    Remove duplicates

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Simple script to run the stock report generator
-
-# Activate virtual environment
-source venv/bin/activate
-
-# Run the command as a module to handle imports properly
-python -m src.main "$@"
-
